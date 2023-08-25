cd70bfd248ed5be33c0de3135c0408d94ec48b39
GustavoPescaroli <130803844+GustavoPescaroli@users.noreply.github.com>
2023-08-25 12:57:23 -0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListaPOOex11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Triangulos a;
+            a = new Triangulos();
+
+            Console.WriteLine("x");
+            a.setx(double.Parse(Console.ReadLine()));
+            Console.WriteLine("y");
+            a.sety(double.Parse(Console.ReadLine()));
+            Console.WriteLine("z");
+            a.setz(double.Parse(Console.ReadLine()));
+            a.calcular();
+        }
+    }
+}
